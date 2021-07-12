6edba44e344a8a486c734b7e8b4c57a474c7f4b9
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2021-07-12 11:34:31 +1000

    initial commit

---
+++ three-displays.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1608x1440 --rotate normal --output DP-1 --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-1 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-2 --off
